ec7a02642d80b670416103cb5b315c98197be312
efadaba <113858922+efadaba@users.noreply.github.com>
2022-10-19 01:29:27 -0700

    updated 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,9 @@
 #nclude "main.h"
 
 /**
-<<<<<<< HEAD
  * main - print _putchar
  * is written by efa
  * Return: 0
-=======
- * main - print putchar
- * Return: always 0
->>>>>>> 480853c6d79bc6c153ad456a1ce98c172c98f0e3
  */
 int main(void)
 {
@@ -21,6 +16,5 @@ int main(void)
     _putchar('a');
     _putchar('r');
     _putchar('\n');
- 
      return (0);
 }
