7619c882cd43a09a5b20b55b7af62527123d459d
efadaba <efadaba110@gmail.com>
2022-10-29 09:18:04 -0700

    my third commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,8 +4,18 @@
  * @n: print int
  * Return: Always 0
  */
+
 void print_number(int n)
 {
+unsigned int x;
 
-return (0);
+x = n;
+if (n < 0)
+{
+_putchar(45);
+x = -n;
+}
+if (x / 10)
+print_number(x / 10);
+_putchar((x % 10) + '0');
 }
