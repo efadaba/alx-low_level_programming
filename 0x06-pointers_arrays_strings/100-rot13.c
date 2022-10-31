057d6a781b446eacd7174e718b11da82e3727fef
efadaba <efadaba110@gmail.com>
2022-10-31 01:43:39 -0700

    my last message

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -13,6 +13,9 @@ char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 while (*(s + count) != '\0')
 {
+
+for (i = 0; i < 52; i++)
+{
 if (*(s + count) == alphabet[i])
 {
 *(s + count) = rot13[i];
